5144e754dd744e6ccffc4707f5d13e4bc1cf88b9
Petr Viktorin <encukou@gmail.com>
2026-05-04 21:53:15 +0200

    Import gnome-shell-frippery-50.0.tgz from http://frippery.org/extensions

--- extensions/Bottom_Panel@rmy.pobox.com/extension.js
+++ extensions/Bottom_Panel@rmy.pobox.com/extension.js
@@ -1322,6 +1322,11 @@ class BottomPanel {
     relayout() {
         let bottom = Main.layoutManager.bottomMonitor;
 
+        // When connecting via RDP we seem to start with null monitors.
+        // Do nothing and hope everything sorts itself out later.
+        if (!bottom)
+            return;
+
         let h = this.actor.get_theme_node().get_height();
         let active = global.workspace_manager.get_active_workspace_index();
         if ( !enable_panel || !show_panel[active] ) h = -h;
